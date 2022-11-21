40f6d6494d531b7fbbc0aa451b085c26477f8003
Glitch (comp584-hw-3) <none>
2022-11-21 19:15:34 +0000

    🎾🏅 Checkpoint ./index.html:48887308/3862 ./script.js:48887308/351

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { tween } from 'popmotion';
+
 const apod_url = "https://api.nasa.gov/planetary/apod";
 const myKey = "?api_key=vPFqHmcIegwQ6lnwUGp701XvW4JcJXf5ZfpiogIJ";
 
@@ -10,6 +12,9 @@ function httpGet(url) {
 
 var apod_json = httpGet(apod_url);
 var apod_array = JSON.parse(apod_json);
-console.log(apod_json);
+console.log(apod_array);
 
 document.getElementById("apod_image").src = apod_array.hdurl;
+document.getElementById("apod_title").innerHTML = apod_array.title;
+document.getElementById("apod_explanation").innerHTML = apod_array.explanation;
+document.getElementById("apod_date").innerHTML = apod_array.date;
